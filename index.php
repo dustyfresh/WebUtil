13adaafde8305b8c07faa46005f9e049522d0d7c
root <root@cveapi.dev>
2015-07-18 07:26:48 +0000

    added Etag support to each method

--- index.php
+++ index.php
@@ -24,128 +24,149 @@ $app->get('/curl/:curlhost', function ($curlhost) {
 
 $app->get('/dig/:dighost', function ($dighost) {
 	$app = \Slim\Slim::getInstance();
-	$data = shell_exec("dig any ".escapeshellarg($dighost));
-	print "<pre>" . $data . "</pre>";
+	$data = "<pre>" . shell_exec("dig any ".escapeshellarg($dighost)) . "</pre>";
+	$app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/ping/:pinghost', function ($pinghost) {
 	$app = \Slim\Slim::getInstance();
-        $data = shell_exec("ping -c4  ".escapeshellarg($pinghost));
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("ping -c4  ".escapeshellarg($pinghost)) . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/whois/:whoishost', function ($whoishost) {
 	$app = \Slim\Slim::getInstance();
-        $data = shell_exec("whois  ".escapeshellarg($whoishost));
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("whois  ".escapeshellarg($whoishost)) . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/mtr/:mtrhost', function ($mtrhost) {
 	$app = \Slim\Slim::getInstance();
-        $data = shell_exec("mtr --report -c 30  ".escapeshellarg($mtrhost));
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("mtr --report -c 30  ".escapeshellarg($mtrhost)) . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/speedtest', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("speedtest");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("speedtest") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/etc/resolv.conf', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("cat /etc/resolv.conf");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("cat /etc/resolv.conf") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/etc/hosts', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("cat /etc/hosts");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("cat /etc/hosts") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/proc/cpuinfo', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("cat /proc/cpuinfo");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("cat /proc/cpuinfo") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/proc/meminfo', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("cat /proc/cpuinfo");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("cat /proc/cpuinfo") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/proc/modules', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("cat /proc/modules");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("cat /proc/modules") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/vmstat', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("vmstat -Sm");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("vmstat -Sm") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/top', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("top -n1 -b");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("top -n1 -b") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/ps', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("ps faux");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("ps faux") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/uptime', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("uptime");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("uptime") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/lsof', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("lsof");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("lsof") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/dmesg', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("dmesg");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("dmesg") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/netstat', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("netstat -natp");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("netstat -natp") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/ifconfig', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("ifconfig");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("ifconfig") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/df', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("df -hT");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("df -hT") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/last', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("last");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>" . shell_exec("last") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->get('/w', function () {
         $app = \Slim\Slim::getInstance();
-        $data = shell_exec("w");
-        print "<pre>" . $data . "</pre>";
+        $data = "<pre>>" . shell_exec("w") . "</pre>";
+        $app->etag(md5($data));
+	print $data;
 });
 
 $app->run();
